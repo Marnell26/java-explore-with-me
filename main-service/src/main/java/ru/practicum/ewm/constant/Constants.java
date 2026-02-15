1ca8b63cfc1fa286f01bd26dedd48a017c55f139
Marnell26 <Marnell26@yandex.ru>
2026-02-15 19:25:03 +0300

    Работа над дипломным проектом. Этап 2 (Основной сервис). Исправления по результатам ревью.

--- main-service/src/main/java/ru/practicum/ewm/constant/Constants.java
+++ main-service/src/main/java/ru/practicum/ewm/constant/Constants.java
@@ -1,6 +1,6 @@
 package ru.practicum.ewm.constant;
 
 public class Constants {
-    public static final String DATE_TIME_FORMAT = "dd.MM.yyyy HH:mm:ss";
+    public static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
 }
